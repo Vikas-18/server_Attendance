1ed1649ac0db0ae32b2bf262aa3613064b288dc7
Vikas-18 <vikaskohli18impossible@gmail.com>
2024-01-25 17:34:40 +0530

    change

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ app.post("/markAttendance", async (req, res) => {
       );
 
       // Set your threshold distance (adjust as needed)
-      const thresholdDistance = 1; // Adjust this value based on your needs
+      const thresholdDistance = 10; // Adjust this value based on your needs
 
       // Check if the user is within the threshold distance of the classroom
       if (distance <= thresholdDistance) {
